dbb82719fe4c53f979eb3aaf233f9d4d2de3ccde
L Carver <wifihum@gmail.com>
2023-01-29 16:45:54 -0800

    add get integer and string input functions

--- movies.cpp
+++ movies.cpp
@@ -8,103 +8,97 @@
 using namespace std;
 
 //------------------------------------------------------------------------------
+// globals
+//------------------------------------------------------------------------------
+// accumulate final rating
+double final_rating = 0;
+bool any_error = false;
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+bool getIntRating();
+bool getStrRating();
+
+//------------------------------------------------------------------------------
 // entry point
 //------------------------------------------------------------------------------
 int main() {
-
-	// accumulate final rating
-	double final_rating = 0;
 
 	// input
 	int i;
 	string s;
 
-	// track bad input
-	bool bad_rating = false;
-	bool bad_actor_input = false;
-	bool bad_others_rating = false;
-	bool bad_thumbed_up = false;
-	bool bad_thumbed_down = false;
+	cout << "It's movie rating time!\n\n";
 
 	cout << "Movie title? ";
 	string title;
 	cin >> title;
 
-	cout << "#1 How do you rate this movie? (1-10) ";
+	cout << "1) How do you rate this movie? (1-10) ";
+	bool bad_rating = getIntRating();
+
+	cout << "2) Do you like movies with this person? ";
+	bool bad_actor_rating = getStrRating();
+
+	cout << "3) How would others rate this movie? (1-10) ";
+	bool bad_others_rating = getIntRating();
+
+	cout << "4) Have you thumbed up this movie already? ";
+	bool bad_thumbed_up = getStrRating();
+
+	cout << "5) Have you thumbed down this movie already? ";
+	bool bad_thumbed_down = getStrRating();
+
+	if (!any_error) {
+		cout << "Final rating: " << final_rating << '\n';
+	}
+	else {
+		// report bad input
+		if (bad_rating)
+			cout << "Invalid rating\n";
+		if (bad_actor_rating)
+			cout << "Invalid actor rating\n";
+		if (bad_others_rating)
+			cout << "Invalid other people's rating\n";
+		if (bad_thumbed_up)
+			cout << "Invalid thumbed up rating\n";
+		if (bad_thumbed_down)
+			cout << "Invalid thumbed down rating\n";
+	}
+}
+
+//------------------------------------------------------------------------------
+// returns true for valid integer input, false otherwise
+//------------------------------------------------------------------------------
+bool getIntRating() {
+	int i;
 	cin >> i;
 
 	if (i >= 1 && i <= 10) {
 		final_rating += i * 0.3;
-	}
-	else {
-		// bad rating
-		bad_rating = true;
+		return false;
 	}
 
-	cout << "#2 Do you like movies with this person? ";
+	any_error = true;
+	return true;
+}
+
+//------------------------------------------------------------------------------
+// returns true for valid string input (yes/no), false otherwise
+//------------------------------------------------------------------------------
+bool getStrRating() {
+	string s;
 	cin >> s;
 
 	if (!s.compare("Yes")) {
 		final_rating += 2;
-	}
-	else if (s.compare("No")) {
-		bad_actor_input = true;
+		return false;
 	}
 
-	cout << "#3 How would others rate this movie? (1-10) ";
-	cin >> i;
-
-	if (i >= 1 && i <= 10) {
-		final_rating += i * 0.3;
+	if (s.compare("No")) {
+		any_error = true;
+		return true;
 	}
-	else {
-		bad_others_rating = true;
-	}
-
-	cout << "#4 Have you thumbed up this movie already? ";
-	cin >> s;
-
-	if (!s.compare("Yes")) {
-		final_rating += 2;
-	}
-	else if (s.compare("No"))	{
-		bad_thumbed_up = true;
-	}
-
-	cout << "#5 Have you thumbed down this movie already? ";
-	cin >> s;
-
-	if (!s.compare("Yes")) {
-		final_rating -= 2;
-	}
-	else if (s.compare("No")) {
-		bad_thumbed_down = true;
-	}
-
-
-	// display the final rating
-	cout << "Final rating: " << final_rating << '\n';
-
-	// here are all the boolean flags we've used so far
-	// we need to check each boolean flag
-
-	//bool bad_rating = false;
-	//bool bad_person_input = false;
-	//bool bad_others_rating = false;
-	//bool bad_thumbed_up = false;
-
-	if (bad_rating)
-		cout << "Bad rating\n";
-	if (bad_actor_input)
-		cout << "Bad other people's rating\n";
-	if (bad_others_rating)
-		cout << "Bad other people's rating\n";
-	if (bad_thumbed_up)
-		cout << "Bad thumbed up rating\n";
-	if (bad_thumbed_down)
-		cout << "Bad thumbed down rating\n";
-
-	// #TODO check for the other two errors
-
 }
 
